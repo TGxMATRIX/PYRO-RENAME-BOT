826d315bc19d8c1c3ee158d53a2b431888d75d62
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-06-21 19:20:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,20 +26,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "28643132")
+    API_HASH  = os.environ.get("API_HASH", "510c3009ea589830a89e045bf8aae656")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5999105365:AAEgOXReN2fAZf79OczgVkBA3MfOQsZJrOU") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","sui:sui")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://sui:sui@cluster0.fmobrpu.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/62472f793d410e849b40b.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1833209093 5845960615 5596825598 6172502825 1957296068').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "MLZ_BOTZ") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001898159090"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
